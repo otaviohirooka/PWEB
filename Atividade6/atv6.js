2debaa4bc24e95f1426cadd76261c045fb557a64
otavioHirooka <otaviolbh@hotmail.com>
2020-05-18 11:43:30 -0300

    Atividade 6

---
+++ Atividade6/atv6.js
@@ -0,0 +1,20 @@
+const Aluno1 = {
+    RA: 0,
+    nome: ""
+}
+
+Aluno1.nome = prompt("Nome do primeiro aluno:")
+Aluno1.RA = prompt("RA do primeiro aluno:")
+
+const Aluno2 = new Object()
+Aluno2.nome = prompt("Nome do segundo aluno:")
+Aluno2.RA = prompt("RA do segundo aluno:")
+
+const Aluno3 = {}
+Aluno3.nome = prompt("Nome do terceiro aluno:")
+Aluno3.RA = prompt("RA do terceiro aluno:")
+
+alert("Primeiro aluno: " + Aluno1.nome + ". RA: " + Aluno1.RA)
+alert("Segundo aluno: " + Aluno2.nome + ". RA: " + Aluno2.RA)
+alert("Terceiro aluno: " + Aluno3.nome + ". RA: " + Aluno3.RA)
+shaka
